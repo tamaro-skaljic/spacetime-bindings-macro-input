d21dc4f17b3cec4e21d26ce869d53508f7da1457
Tamaro Skaljic <49238587+tamaro-skaljic@users.noreply.github.com>
2026-09-23 07:42:37 +0200

    add reducer so that the example compiles

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,4 +1,5 @@
 use spacetime_bindings_macro_input_example_macros::test;
+use spacetimedb::{procedure, reducer};
 
 #[test]
 #[spacetimedb::table(accessor = entity, public)]
@@ -9,4 +10,10 @@ pub struct Entity {
     id: u128,
 
     created_at: spacetimedb::Timestamp,
+}
+
+// This reducer is required, otherwise the SpacetimeDB module wouldn't compile. ("Error: unable to determine ABI of module (may be on an spacetime version < 0.8)")
+#[reducer]
+pub fn test(_: &spacetimedb::ReducerContext) -> Result<(), Box<str>> {
+    Ok(())
 }
